c4f6cff18c88fb0253ac282ee9dea9599b197d16
AlainNem <alain.nemnom128@gmail.com>
2025-04-11 15:12:35 +0200

    computed props and add primevue icon library

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup>
- import NavBar from './components/NavBar.vue';
- import Hero from './components/Hero.vue';
- import HomeCards
-  from './components/HomeCards.vue';
-  import JobsListings from './components/JobsListings.vue';
-
+import NavBar from './components/NavBar.vue';
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobsListings from './components/JobsListings.vue';
 </script>
 
 <template>
   <NavBar></NavBar>
   <Hero title="Vue js application" subtitle="First VueJs application"></Hero>
   <HomeCards/>
-  <JobsListings/>
+  <JobsListings :limit="3" :showButton=true />
 </template>
