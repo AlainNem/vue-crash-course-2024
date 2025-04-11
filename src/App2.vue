546eb735297a3495c078c37421ddabfa155b0bd3
AlainNem <KBCD4417@orange.com>
2025-04-11 11:02:59 +0200

    switch to composition API syntax

---
+++ src/App2.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('alain');
+const status = ref('active');
+const tasks =ref(['task1','task2','task3','task4']);
+const toggleStatus = () => {
+  if(status.value ==='active'){
+    status.value = 'pending'
+  } else if(status.value ==='pending'){
+    status.value = 'inactive'
+  } else{
+    status.value = 'active'
+  }
+}
+</script>
+
+<template>
+  <h1>{{name}}</h1>
+  <p v-if="status==='active'">User is active</p>
+  <p v-else-if="status==='pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" v-bind:key="task">
+      {{ task }}
+    </li>
+  </ul>
+  <br/>
+  <button @click="toggleStatus">Change status</button>
+</template>
+
