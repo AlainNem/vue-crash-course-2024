e30b8e6f47fa4c054e4e76817cfdc30bf96d3010
AlainNem <alain.nemnom128@gmail.com>
2025-04-14 13:58:30 +0200

    fix front proxy

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server:{
    proxy: {
     '/api':{
-      target: 'https://json-server-eh7v.onrender.com',
+      target: 'http://localhost:5000',
       changeOrigin: true,
       rewrite: (path) => path.replace(/^\/api/,''),
     }
