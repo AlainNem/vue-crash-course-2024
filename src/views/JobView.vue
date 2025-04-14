675b76a2f5dc3b5d27878cae67a845c4fe617f5a
AlainNem <alain.nemnom128@gmail.com>
2025-04-14 10:15:28 +0200

    add proxy

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -15,7 +15,7 @@ const state = reactive({
 
 onMounted(async () => {
     try {
-        const response = await axios.get(`http://localhost:5000/jobs/${JobId}`);
+        const response = await axios.get(`/api/jobs/${JobId}`);
         state.job = response.data;
     } catch (error) {
         console.error('Error fetching job', error);
